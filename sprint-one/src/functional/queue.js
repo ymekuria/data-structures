ddf400de3f9a6732f9a4892ac9a581cc209f7b9f
ymekuria <ymekuria@gmail.com>
2015-11-18 15:25:17 -0800

    'Completed and passed queue shared behaviour and functional instantiation stle tests'

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,24 @@
 var Queue = function() {
   var someInstance = {};
-
+  var stackSize = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    stackSize++;
+    someInstance[stackSize] = value;
   };
 
   someInstance.dequeue = function() {
+    var poppedValue = someInstance["1"]
+    stackSize--;
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    return stackSize >= 0 ? stackSize : 0;
   };
 
   return someInstance;
