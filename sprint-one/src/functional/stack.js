da005ad08f590e5d22221f3bd1ad8404ed96dacf
ymekuria <ymekuria@gmail.com>
2015-11-18 15:12:41 -0800

    'Implemented and passed all the tests for The functional pattern stack'

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
+  var stackSize = 0;
+
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    
+    stackSize++;
+    someInstance[stackSize] = value;
   };
 
   someInstance.pop = function() {
+    var myValue = someInstance[stackSize];
+    stackSize--;
+    return myValue;
   };
 
   someInstance.size = function() {
+    return stackSize >= 0 ? stackSize : 0;
   };
 
   return someInstance;
