fb606460b95b006f540925cf7735099f09a32918
ymekuria <ymekuria@gmail.com>
2015-11-19 12:24:13 -0800

    'Completed all the tests for the pseudoclassical pattern'

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -17,14 +17,14 @@ Queue.prototype.enqueue = function(value){
 Queue.prototype.dequeue = function(){
   var poppedValue = this[this.firstValue];
   delete this[this.firstValue];
-  this.stackIndex++;
-  //this.firstValue++;
+ // this.stackIndex++;
+  this.firstValue++;
   this.stackSize > 0 ? this.stackSize-- : 0; 
-  if(this.stackSize === 0) {
-    this.firstValue = 1;
-  } else{
-    this.firstValue++
-  }
+  // if(this.stackSize === 0) {
+  //   this.firstValue = 1;
+  // } else{
+  //   this.firstValue++
+  // }
  // debugger;
   return poppedValue;
 };
