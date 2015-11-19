89cd71b8356e96f206df03aa2e9be4c138228d5e
ymekuria <ymekuria@gmail.com>
2015-11-19 10:54:37 -0800

    'Completed all the tests for the prototypal pattern'

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+ 
+
+  var stack = Object.create(stackMethods);
+  stack.stackSize = 0;
+  stack.firstValue = 1;
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value){
+    this.stackSize++;
+    this[this.stackSize] = value;
+
+  },
+  pop: function(){
+    var poppedValue = this[this.stackSize];
+    this.stackSize > 0 ? this.stackSize-- : 0;
+    return poppedValue;
+  },
+  size: function(){
+    return this.stackSize;
+  }
+};
 
 
