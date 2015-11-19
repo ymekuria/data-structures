716c897af20e48d87d2c24343a790bf236db05bd
ymekuria <ymekuria@gmail.com>
2015-11-18 16:23:55 -0800

    'Completed all the tests for the functional pattern'

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,24 +1,41 @@
 var Queue = function() {
   var someInstance = {};
+  var size = 0;
   var stackSize = 0;
+  var firstValue = 1;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
     stackSize++;
     someInstance[stackSize] = value;
+    console.log('someInstance inside enqueue:', someInstance)
   };
 
   someInstance.dequeue = function() {
-    var poppedValue = someInstance["1"]
-    stackSize--;
+    var poppedValue = someInstance[firstValue];
+    console.log("someInstance[firstValue]", someInstance[firstValue])
+    console.log("first value: ", firstValue)
+     console.log("poppedValue: ",poppedValue);
+
+
+    size --;
+    if(stackSize === 0) {
+      firstValue = 1;
+
+    } else{
+      firstValue++;
+    }
+    
+   
     return poppedValue;
   };
 
   someInstance.size = function() {
-    return stackSize >= 0 ? stackSize : 0;
+    return size >= 0 ? size : 0;
   };
 
   return someInstance;
