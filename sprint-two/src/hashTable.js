1b1a91e7bb7e658936a347a45d6f815694b84c2e
ymekuria <ymekuria@gmail.com>
2015-11-20 12:32:32 -0800

    'Implemented hash table and passed 4 of the 6 tests'

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,21 +3,28 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, [k,v]);
+ 
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var keyValue =  this._storage.get(index);
+  return keyValue[1];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
-
+var hashTable = new HashTable();
 
 /*
  * Complexity: What is the time complexity of the above functions?
